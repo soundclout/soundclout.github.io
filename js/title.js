c05c9f29204cfc291c0ce6947b0fe87a5c2decec
soundclout <xbot2@protonmail.com>
2017-08-17 21:18:58 -0500

    Update title.js

--- js/title.js
+++ js/title.js
@@ -1,25 +1,25 @@
 if(document.addEventListener){
-	document.addEventListener("DOMContentLoaded", function(){
-		loaded();
-	});
+    document.addEventListener("DOMContentLoaded", function(){
+        loaded();
+    });
 } else if(document.attachEvent){
-	document.attachEvent("onreadystatechange", function(){
-		loaded();
-	});
+    document.attachEvent("onreadystatechange", function(){
+        loaded();
+    });
 }
 
 function loaded(){
 
-	setInterval(loop, 600);
+    setInterval(loop, 600);
 
 }
 
 var x = 0;
 
-var titleText = ["b", "bd", "bdo", "bdog"];
+var titleText = ["s", "o", "u", "n", "d", "c", "l", "o", "u", "t", "soundclout"];
 
 function loop(){
 
-	document.getElementsByTagName("title")[0].innerHTML = titleText[x++%titleText.length];
+    document.getElementsByTagName("title")[0].innerHTML = titleText[x++%titleText.length];
 
 }
