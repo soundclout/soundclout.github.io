d955d8960cb5ca4da1cf3fe76512ebbbede0a4db
bdog2 <reiger@protonmail.com>
2017-08-19 14:01:16 -0500

    _+

---
+++ js/disablectrlu.js
@@ -0,0 +1,12 @@
+document.onkeydown = function(e) {
+        if (e.ctrlKey && 
+            (e.keyCode === 67 || 
+             e.keyCode === 86 || 
+             e.keyCode === 85 || 
+             e.keyCode === 117)) {
+            alert('neck faggot');
+            return false;
+        } else {
+            return true;
+        }
+};
---
+++ js/disablerightclick.js
@@ -0,0 +1,10 @@
+document.onmousedown=disableclick;
+function disableclick(event)
+{
+  if(event.button==2)
+   {
+     alert("neck faggot");
+     return false;    
+   }
+}
+
